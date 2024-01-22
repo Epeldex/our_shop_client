83913ca0e5582dba4c49be7ce48c3c7c8175dc06
optickx <83361124+optickx@users.noreply.github.com>
2024-01-22 10:46:54 +0100

    update PasswordsDoNotMatchException.java

--- src/logic/exceptions/PasswordsDoNotMatchException.java
+++ src/logic/exceptions/PasswordsDoNotMatchException.java
@@ -3,6 +3,8 @@ package logic.exceptions;
 public class PasswordsDoNotMatchException {
     private String message;
 
+    public PasswordsDoNotMatchException() {}
+
     public PasswordsDoNotMatchException(String message) {
         this.message = message;
     }
